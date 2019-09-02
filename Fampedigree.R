74ec23b32512ccd5bdc468c3920f496ee0d42c29
Nicholeyang0215 <40403049+Nicholeyang0215@users.noreply.github.com>
2019-09-02 12:01:46 -0500

    Update Fampedigree.R

--- Fampedigree.R
+++ Fampedigree.R
@@ -1,15 +1,11 @@
-
-
-# Script to generate N family pedigree data and calculate carrier scores for all probands. 
+## Script to generate N family pedigree data and calculate carrier scores for all probands. 
 
 getwd()
 library(BMmultigene)
 library(BayesMendel)
 
-
 seed <- round(runif(1, 0, 5000))
 set.seed(seed)
-
 
 # Consider breast cancer and ovarian cancer
 cancers = c("BC", "OC")
@@ -20,10 +16,10 @@ mutations = c("BRCA1", "BRCA2")
 # BRCA1 and BRCA2 prevalences 
 prevs = c(BRCA1=0.05, BRCA2=0.05) 
 
-# Load data
+## Load data
 data(BRCApenet.metaDSL.2008) 
 
-# Get penetrance corresponding to noncarriers, BRCA1 carriers, and BRCA2 carriers
+## Get penetrance corresponding to noncarriers, BRCA1 carriers, and BRCA2 carriers
 # Females 
 penCancersF = list(BC=BRCApenet.metaDSL.2008$fFX[,c(1,2,4)], # Breast cancer
                    OC=BRCApenet.metaDSL.2008$fFY[,c(1,2,4)]) # Ovarian cancer
@@ -33,7 +29,7 @@ penCancersM = list(BC=BRCApenet.metaDSL.2008$fMX[,c(1,2,4)], # Breast cancer
 
 data(death.othercauses) 
 
-# Males don't get ovarian cancer, so there is no death from other causes
+## Males don't get ovarian cancer, so there is no death from other causes
 # Use death from other causes for female ovarian cancer. 
 deathOtherCauses = death.othercauses[,c(2,1,3,3)] 
 names(deathOtherCauses) = c("femaleBC", "maleBC", "femaleOC", "maleOC")
@@ -62,7 +58,7 @@ nSibs = c(2,3) # Proband has two sisters and three brothers
 nGrandchild = c(2,1) # Each person in proband's generation has two daughters and a son 
 
 
-# Generation of one family pedigree with carrier scores.
+## Generation of one family pedigree with carrier scores.
 # Function sim.simFam generates a completed family matrix 
 fam = sim.simFam(nSibsPatern, nSibsMatern, nSibs, nGrandchild, prevs, CP, includeGeno=TRUE)
 # Function eskLik estimates the likelihood matrix, in other words, calculate probands carrier probability. 
