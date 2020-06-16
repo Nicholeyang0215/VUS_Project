1f454195e6d2546f32a781a20c14880342862a91
Nicholeyang0215 <40403049+Nicholeyang0215@users.noreply.github.com>
2020-06-15 20:09:52 -0500

    Update Figure4.R

--- Results/Figure4.R
+++ Results/Figure4.R
@@ -1,52 +1,63 @@
 
-## Script to visualize simulation results of positive predictive value. 
+## Script to visualize simulation results of positive predictive value (PPV). 
 
-# create sequence of carrier scores, matching the length of row of dt_ppv.
+
+# load PPVs for all simulated cohorts
+setwd("/Users/nicholeyang/Desktop/research2/VUSpaper_Revision/new_sim_results/Fig4/results")
+res.list = list.files(pattern="ppv*")
+
+n = 2000 ## number of replicates
+
+# create carrier scores for the same length. 
 sequence = seq(-9, 10, by = 0.01)
 
-## Load data, dt_ppv
-# dt_ppv stores all simulation results of positive predictive value (PPV). 
-# Columns of dt_ppv: simulation replicates(1828 in total). 
-# Rows of dt_ppv: created based on carrier score of the above sequence. PPVs in each replicate are stored in dt_ppv starting from certain rows according to the carrier score. 
-# NAs in a column: in that simulation replicate, we don't have carrier score of that value. 
-load("/Users/nicholeyang/Desktop/research2/github code/Data/dt_ppv.RData")
+# create matrix to store postivie predictive value(dt_ppv). Columns represent replicates.
+dt_ppv = matrix(NA, ncol = n, nrow = length(sequence)) 
+
+## fill ppv into dt_ppv
+for (i in 1:n){
+  
+  load(res.list[i])
+
+  scores2 = round(scores,2)  ## round simulated scores to 2-digit
+  indx = which(sequence == scores2[1])   ## find the place to start 
+  
+  for (j in c(1:length(scores2))){
+  
+    dt_ppv[indx-1+j,i] = ppv[j]
+  
+    }
+}
 
 
-## calculate means of ppv (all replicates)
-means = apply(dt_ppv,1, mean,na.rm = T)
-
-## calculate 0.05 and 0.95 quantiles of ppv (all replicates)
-lower_q = apply(dt_ppv, 1, function(x) quantile(x,0.05,na.rm =T))
-upper_q = apply(dt_ppv, 1, function(x) quantile(x,0.95,na.rm =T))
+## keep carrier score ppv values with more than 25% observations in 2000 replicates. 
+NA_replicates = apply(dt_ppv, 1, function(x) sum(is.na(x)))
+sub_ppv = dt_ppv[NA_replicates < 1500, ]
 
 
-## remove NAs (Rows that are all NAs)
-lower_q2 = lower_q[!is.na(lower_q)]
-upper_q2 = upper_q[!is.na(lower_q)]
-means2 = means[!is.na(means)]
+## calculate means and CI(0.05, 0.95 quantiles) for ppv
+means = apply(sub_ppv,1, mean,na.rm = T)
+lower_q = apply(sub_ppv, 1, function(x) quantile(x,0.05,na.rm =T))
+upper_q = apply(sub_ppv, 1, function(x) quantile(x,0.95,na.rm =T))
 
 
-## get the range of carrier score values with most simulation replicates 
-x = sequence[!is.na(means)]
-
-x1 = x[85:1285]
-s1 = exp(x[85:1285])/(1+exp(x[85:1285]))   ## carrier scores on the original scale
-y1 = means2[85:1285]    ## means of PPV without NAs 
-y2 = lower_q2[85:1285]  ## lower bond of PPV without NAs 
-y3 = upper_q2[85:1285]  ## upper bond of PPV without NAs
-
+x = sequence[NA_replicates < 1500]
+s = exp(x)/(1+exp(x))   ## carrier scores on the original scale
+y1 = means   ## means of PPV
+y2 = lower_q  ## lower bond of ppv 
+y3 = upper_q ## upper bond of ppv 
 
 
 ## Code for plot
 par(mfrow= c(1,2))
 
-# Plot of PPV on original scale, with CI
-plot(s1, y1, type = 'p', pch = 19,cex = 0.1, ylab = "Positive Predictive Value", xlab = "(a) Carrier Probabilities")
-polygon(c(s1,rev(s1)),c(y2,rev(y3)),col="gray80", border="gray87")
-lines(s1, y1, type = 'p', pch = 19,cex = 0.1, col = "black")
+## plot of ppv, with CI
+plot(s, y1, type = 'p', pch = 19,cex = 0.1, ylab = "Positive Predictive Value", xlab = "(a) Carrier Scores on Original Scale")
+polygon(c(s,rev(s)),c(y2,rev(y3)),col="gray80", border="gray87")
+lines(s, y1, type = 'p', pch = 19,cex = 0.1, col = "black")
 
 
-# Plot of PPV on logit scale, with CI
-plot(x1, y1, type = 'p', pch = 19,cex = 0.1, ylab = "Positive Predictive Value", xlab = "(b) Logit Carrier Probabilities")
-polygon(c(x1,rev(x1)),c(y2,rev(y3)),col="gray80", border="gray87")
-lines(x1, y1, type = 'p', pch = 19,cex = 0.1, col = "black")
+## plot of ppv, with CI
+plot(x, y1, type = 'p', pch = 19,cex = 0.1, ylab = "Positive Predictive Value", xlab = "(b) Carrier Scores on Logit Scale")
+polygon(c(x,rev(x)),c(y2,rev(y3)),col="gray80", border="gray87")
+lines(x, y1, type = 'p', pch = 19,cex = 0.1, col = "black")
