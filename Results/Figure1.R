a874dc3f11dc58a2d3f4ac91b66cd9f327630449
Nicholeyang0215 <40403049+Nicholeyang0215@users.noreply.github.com>
2020-06-15 19:53:06 -0500

    Update Figure1.R

--- Results/Figure1.R
+++ Results/Figure1.R
@@ -1,3 +1,4 @@
+
 ## Script to generate Figure 1.
 getwd()
 
@@ -6,86 +7,12 @@ library(BayesMendel)
 library(pracma)
 library(maxLik)
 
-seed <- round(runif(1, 0, 5000))
-set.seed(seed)
+# load a simulated cohort of sample size = 5000
+load("../5k_fam1.RData")
 
-
-## Simulate 5000 family pedigrees and calculate probands' carrier probabilities. 
-# Consider breast cancer and ovarian cancer
-cancers = c("BC", "OC")
-
-# Consider BRCA1 and BRCA2 mutations
-mutations = c("BRCA1", "BRCA2")
-
-# BRCA1 and BRCA2 prevalences 
-prevs = c(BRCA1=0.05, BRCA2=0.05) 
-
-# Load data
-data(BRCApenet.metaDSL.2008) 
-
-# Get penetrance corresponding to noncarriers, BRCA1 carriers, and BRCA2 carriers
-# Females 
-penCancersF = list(BC=BRCApenet.metaDSL.2008$fFX[,c(1,2,4)], # Breast cancer
-                   OC=BRCApenet.metaDSL.2008$fFY[,c(1,2,4)]) # Ovarian cancer
-# Males
-penCancersM = list(BC=BRCApenet.metaDSL.2008$fMX[,c(1,2,4)], # Breast cancer
-                   OC=BRCApenet.metaDSL.2008$fMY[,c(1,2,4)]) # Ovarian cancer
-
-data(death.othercauses) 
-
-# Males don't get ovarian cancer, so there is no death from other causes
-# Use death from other causes for female ovarian cancer. 
-deathOtherCauses = death.othercauses[,c(2,1,3,3)] 
-names(deathOtherCauses) = c("femaleBC", "maleBC", "femaleOC", "maleOC")
-
-# Cancer
-CP = genCancerPen(mutations, cancers, penCancersF, penCancersM)
-
-# Other causes
-ODP = genOtherDeathPen(cancers, deathOtherCauses)
-
-# Load data
-data(compriskSurv)
-
-# Separate competing risk by gender
-comprisk = list(Female=compriskSurv[,1:4], Male=compriskSurv[,5:8])
-
-# Rename columns to match possible genotypes
-colnames(comprisk$Female) = CP$PG 
-colnames(comprisk$Male) = CP$PG
-
-# Specify number of males and females in each branch of the family. 
-nSibsPatern = c(1,2) # One paternal aunt and two paternal uncles 
-nSibsMatern = c(2,2) # Two maternal aunts and two maternal uncles
-nSibs = c(2,3) # Proband has two sisters and three brothers 
-nGrandchild = c(2,1) # Each person in proband's generation has two daughters and a son 
-
-
-fam = sim.simFam(nSibsPatern, nSibsMatern, nSibs, nGrandchild, prevs, CP, includeGeno=TRUE)
-lik = estLik(fam, CP, ODP, comprisk)
-res= data.frame(pp.peelingParing(fam, prevs, lik, 2)) 
-
-for (i in 2:5e3){
-  
-  fam_new = sim.simFam(nSibsPatern, nSibsMatern, nSibs, nGrandchild, prevs, CP, includeGeno=TRUE)
-  lik = estLik(fam_new, CP, ODP, comprisk)
-  res_new= data.frame(pp.peelingParing(fam_new, prevs, lik, 2))
-  
-  fam = rbind(fam,fam_new)
-  res = cbind(res,res_new)
-}
-
-res2 = t(res)
-carrier_prob = 1-res2[,1]
-proband = fam[which(fam$isProband==1),]
-
-fam.id = c(1:5e3)
-res.all = cbind(fam.id,proband,carrier_prob)
+res.all = res.sub
 non_carrier = res.all[which(res.all$BRCA1==0 & res.all$BRCA2==0), ]
 carrier = res.all[which(res.all$BRCA1==1 | res.all$BRCA2==1),]
-
-
-
 
 
 ## Code for plotting Figure 1. 
@@ -138,10 +65,10 @@ legend("topright",legend = c("Positive", "Negative","VUS"), col = c(1:3), pch = 
 
 
 
-
 ## Carrier score distribution when theta = 0.05
 true_pi = 0.1
 prop_VUS = 0.3
+theta = 0.05
 
 n_pos = nrow(res.all)*prop_VUS*true_pi
 n_neg = nrow(res.all)*prop_VUS*(1-true_pi)
@@ -156,7 +83,7 @@ VUS = rbind(carrier[index1,], non_carrier[index2,])
 pos = carrier[-index1,]
 neg = non_carrier[-index2,]
 
-theta = 0.05
+
 n_falneg = round(nrow(neg)*theta/(1-theta))
 famid3 = sample(pos$fam.id, n_falneg, replace = FALSE)
 index3 = which(pos$fam.id %in% famid3)
